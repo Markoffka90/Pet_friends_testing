726a266b489ae510a86b75a9526b79a8794b3379
Markoffka90 <annet44_91@mail.ru>
2023-07-31 14:38:40 +0300

    Tests for PetFriends v2 add new metods

--- api.py
+++ api.py
@@ -92,3 +92,40 @@ class PetFriends:
         except json.decoder.JSONDecodeError:
             result = res.text
         return status, result
+
+    def add_new_pet_without_photo(self, auth_key: json, name: str, animal_type: str,
+                    age: str) -> json:
+        """Метод отправляет (постит) на сервер данные о добавляемом питомце и возвращает статус
+        запроса на сервер и результат в формате JSON с данными добавленного питомца без фото"""
+
+        data = {
+                'name': name,
+                'animal_type': animal_type,
+                'age': age
+            }
+        headers = {'auth_key': auth_key['key']}
+
+        res = requests.post(self.base_url + 'api/create_pet_simple', headers=headers, data=data)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        return status, result
+
+    def add_pet_photo(self, auth_key: json,  pet_id: str, pet_photo: str) -> json:
+        """Метод отправляет (постит) на сервер фото питомца и возвращает статус
+        запроса на сервер и результат в формате JSON с данными питомца"""
+
+        file = {'pet_photo': (pet_photo, open(pet_photo, 'rb'), 'image/jpeg')}
+        headers = {'auth_key': auth_key['key']}
+
+        res = requests.post(self.base_url + f'api/pets/set_photo/{pet_id}', headers=headers, files=file)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        return status, result
--- tests/test_pet_friends.py
+++ tests/test_pet_friends.py
@@ -84,3 +84,40 @@ def test_successful_update_self_pet_info(name='Mars', animal_type='sfinks', age=
     else:
         # если спиок питомцев пустой, то выкидываем исключение с текстом об отсутствии своих питомцев
         raise Exception("There is no my pets")
+
+def test_add_new_pet_with_valid_data_without_photo(name='Plux', animal_type='cat', age='3'):
+    """Проверяем что можно добавить питомца с корректными данными простым способом"""
+
+    # Запрашиваем ключ api и сохраняем в переменую auth_key
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+
+    # Добавляем питомца
+    status, result = pf.add_new_pet_without_photo(auth_key, name, animal_type, age)
+
+    # Сверяем полученный ответ с ожидаемым результатом
+    assert status == 200
+    assert result['name'] == name
+
+def test_add_photo_for_pet(pet_photo='images/plux.jpg'):
+    """Проверяем что можно добавить фото питомца"""
+
+    # Запрашиваем ключ api и сохраняем в переменую auth_key
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+
+    # Запрашиваем список своих питомцев
+    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+
+    # Проверяем - если список своих питомцев пустой, то добавляем нового и опять запрашиваем список своих питомцев
+    if len(my_pets['pets']) == 0:
+        pf.add_new_pet_without_photo(auth_key, "Plux", "cat", "2")
+        _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+
+    # Берём id первого питомца из списка
+    pet_id = my_pets['pets'][0]['id']
+
+    # Добавляем фото питомца
+    status, result = pf.add_pet_photo(auth_key, pet_id, pet_photo)
+
+    # Сверяем полученный ответ с ожидаемым результатом
+    assert status == 200
+    assert result['pet_photo'] != ''
